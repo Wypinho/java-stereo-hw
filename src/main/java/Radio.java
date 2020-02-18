2e6b1b640a66be9e721bb9c7ab20885271f60d92
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-02-18 17:52:55 +0000

    radio tests pass, creating other components

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,2 +1,16 @@
 public class Radio {
+    private String station;
+
+    public Radio(){
+        this.station = "";
+    }
+
+    public String getStation() {
+        return station;
+    }
+
+
+    public void tune(String station) {
+        this.station = station;
+    }
 }
